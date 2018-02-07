9ebdd9d5236d3a76ac2d3f89b6911bb48ae9719a
Jen Saxena <jen@jensaxena.com>
2018-02-07 17:16:59 -0500

    Add ball

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,12 +1,54 @@
 // NAMESPACE
 const smash = {};
 
+// CREATE & ACCESS CANVAS
 smash.canvas = document.getElementById('canvas');
-smash.context = smash.canvas.getContext("2d");
+smash.context = smash.canvas.getContext('2d');
+
+// SET & RESET DEFAULT GAME STATE
+smash.x = undefined;
+smash.y = undefined;
+
+smash.dx = undefined;
+smash.dy = undefined;
+
+smash.default = function() {
+  smash.x = smash.canvas.width / 2;
+  smash.y = smash.canvas.height - 30;
+  smash.dx = 2;
+  smash.dy = -2;
+};
+
+// CREATE BALL
+smash.ballRadius = 10;
+
+smash.drawBall = function() {
+  smash.context.beginPath();
+  smash.context.arc(smash.x, smash.y, smash.ballRadius, 0, Math.PI * 2);
+  smash.context.fillStyle = 'white';
+  smash.context.fill();
+  smash.context.closePath();
+};
+
+// ACTION!
+smash.draw = function() {
+
+  // CLEAR CANVAS
+  smash.context.clearRect(0, 0, smash.canvas.width, smash.canvas.height);
+
+  smash.drawBall();
+
+  // MOVE
+  smash.x += smash.dx;
+  smash.y += smash.dy;
+
+  requestAnimationFrame(smash.draw);
+};
 
 // INIT
 smash.init = function() {
-
+  smash.default();
+  smash.draw();
 };
 
 // DOCREADY
